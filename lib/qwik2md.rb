47f37d5a8fa9a2b265e6261aa24aee58e76a5723
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-03-16 18:52:22 +0900

    Convert to md

--- lib/qwik2md.rb
+++ lib/qwik2md.rb
@@ -3,14 +3,35 @@ require "qwik/parser-emode"
 require "qwik/tokenizer"
 require "qwik/parser"
 require 'qwik/wabisabi-format-xml'
+require 'reverse_markdown'
 
 module Qwik2md
   class << self
-    def convert(qwik)
-      if Qwik::EmodePreProcessor.emode?(qwik)
-	       qwik = Qwik::EmodePreProcessor.preprocess(qwik)
-      end
-      tokens = Qwik::TextTokenizer.tokenize(qwik)
+    def convert(qwik_str)
+      Converter.new(qwik_str).convert
+    end
+  end
+
+  class Converter
+    def initialize(qwik_str)
+      @qwik_str = qwik_str
+    end
+
+    def convert
+      ReverseMarkdown.convert(to_html)
+    end
+
+    private
+
+    def to_html
+      str =
+        if Qwik::EmodePreProcessor.emode?(@qwik_str)
+           Qwik::EmodePreProcessor.preprocess(@qwik_str)
+         else
+           @qwik_str
+        end
+
+      tokens = Qwik::TextTokenizer.tokenize(str)
       tree = Qwik::TextParser.make_tree(tokens)
       tree.format_xml
     end
--- test/qwik2md_test.rb
+++ test/qwik2md_test.rb
@@ -1,17 +1,16 @@
 require 'test_helper'
 
 class Qwik2mdTest < Minitest::Test
-  def test_hoge
-    expected = <<~HTML.chomp
-      <h2
-      >h2</h2
-      ><h3
-      >h3</h3
-      >
-    HTML
+  def test_convert
+    expected = <<~MD
+      ## h2
+
+      ### h3
+    MD
 
     actual = Qwik2md.convert(<<~QWIK)
       * h2
+
       ** h3
     QWIK
 
